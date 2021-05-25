9200fd4dff06c4d2a7a143cf000449297e22975c
Tadeo Palafox <palafot@miamioh.edu>
2021-05-24 23:08:20 -0400

    Implemented Home Page

--- src/db/index.js
+++ src/db/index.js
@@ -1,2 +1,5 @@
 export { db } from './db';
+export { deleteMeal } from './deleteMeal';
+export { deleteIngredient } from './deleteIngredient';
 export { getPopulatedMeals } from './getPopulatedMeals';
+export { getIngredients } from './getIngredients';
