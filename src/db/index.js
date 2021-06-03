25720b822eb12d203f56eb802350b7a4001eea92
Tadeo Palafox <palafot@miamioh.edu>
2021-06-03 15:15:35 -0400

    Implemented Recipe search page

--- src/db/index.js
+++ src/db/index.js
@@ -4,3 +4,5 @@ export { deleteIngredient } from './deleteIngredient';
 export { getPopulatedMeals } from './getPopulatedMeals';
 export { getIngredients } from './getIngredients';
 export { insertIngredient } from './insertIngredient';
+export { insertMeal } from './insertMeal';
+export { searchRecipes } from './searchRecipes';
---
+++ src/db/insertMeal.js
@@ -0,0 +1,7 @@
+import { db } from './db';
+
+export const insertMeal = async (meal) => {
+    const connection = db.getConnection();
+    await connection.collection('meals')
+        .insertOne(meal);
+}
